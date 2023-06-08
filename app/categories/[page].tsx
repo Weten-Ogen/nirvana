c49842949be18f26af196c42cdd236b4f75b9b20
Weten-Ogen <alphaoware@gmail.com>
2023-06-08 03:20:49 +0000

    bug fixed

--- app/categories/[page].tsx
+++ app/categories/[page].tsx
@@ -5,11 +5,8 @@ import React from 'react'
 
 
 export default async function Categories({params}:any) {
-    const response = await fetch(` https://newsapi.org/v2/top-headlines?country=us?category=${category}&apiKey=${process.env.Api_key}`,{
-        next:{
-            revalidate:20
-        }
-    })
+    const {category} = params.id
+    const response = await fetch(` https://newsapi.org/v2/top-headlines?country=us?category=${category}&apiKey=${process.env.Api_key}`)
     const newArticles = await response.json()
    
 
